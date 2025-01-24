e09fbd57b52f29124125e6575903331a04a4892a
igor-romasiuk <igor.romasiuk@gmail.com>
2025-01-24 17:12:22 +0200

    configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/rick-and-morty',
+  assetPrefix: '/rick-and-morty'
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
