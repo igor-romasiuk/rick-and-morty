d9a720e42dd11c08aa0cd0dfe26a35b4df03253a
igor-romasiuk <igor.romasiuk@gmail.com>
2025-03-18 13:00:20 +0200

    Implement authentication middleware for protected routes

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token
+  },
+  pages: {
+    signIn: "/login"
+  }
+})
+
+export const config = {
+  matcher: [
+    "/profile/:path*",
+    "/api/profile/:path*"
+  ]
+} 
