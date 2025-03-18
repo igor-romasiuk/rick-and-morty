0cdce059c4800dfe2ad860b817da94aa04efe06c
igor-romasiuk <igor.romasiuk@gmail.com>
2025-03-18 13:00:03 +0200

    Define application constants and navigation paths

---
+++ constants/api.ts
@@ -0,0 +1,8 @@
+export const API_BASE_URL = "https://rickandmortyapi.com/api";
+export const CACHE_DURATION = 5 * 60 * 1000;
+
+export const API_ENDPOINTS = {
+  CHARACTERS: `${API_BASE_URL}/character`,
+  EPISODES: `${API_BASE_URL}/episode`,
+  LOCATIONS: `${API_BASE_URL}/location`,
+}; 
